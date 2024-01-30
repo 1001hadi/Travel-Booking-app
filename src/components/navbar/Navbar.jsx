9b647d26c8a0445c5f6d8594de094b9ef3c60b85
1001hadi <hadi.jaf.1001@gmail.com>
2024-01-30 09:55:09 -0700

    data has been fatched from server to UI

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">JAF Booking</span>
+        <Link to="/" className="home">
+          <span className="logo">JAF Booking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
